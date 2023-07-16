deaf7f4e68c5f6edc2a64997b9f25891c6b9e007
Brooke Guajardo <brooke.guajardo@gmail.com>
2023-07-16 00:25:13 -0500

    adding pause, dialogue logic

---
+++ Scenes/UI/new_pause.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+#const main_menu := "res://scenes/assets_only/main_menu/main_menu.tscn"
+#const loading := "res://scenes/assets_only/loading/loading.tscn"
+
+func _ready():
+	get_tree().paused = true
+
+func _on_resume_pressed():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	get_tree().paused = false
+	queue_free()
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	#get_tree().change_scene_to_file(main_menu)
--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -1,6 +1,8 @@
 #Script for Non-Player-Characters
 extends Character
 
+# var resource = load("res://scenes/main/blue_man.dialogue")
+
 func _ready():
 	pass
 
@@ -9,3 +11,10 @@ func _physics_process(delta):
 
 func _on_interact():
 	print (self.name + " has been interacted with")
+
+#func _on_player_touch(object):
+#	if object == self:
+#		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+#		var dialogue_instance = preload("res://scenes/main/dialogue_window.tscn").instantiate()
+#		dialogue_instance.resource = resource
+#		add_child(dialogue_instance)
