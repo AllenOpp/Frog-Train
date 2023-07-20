b515f9eb3f18eefc1c7fe4d8118790661995ca21
Brooke Guajardo <brooke.guajardo@gmail.com>
2023-07-20 16:57:33 -0500

    textures and state

--- Scripts/NPCIdle.gd
+++ Scripts/NPCIdle.gd
@@ -2,17 +2,25 @@ extends State
 class_name NPCIdle
 
 @export var npc: CharacterBody3D
+<<<<<<< HEAD
 @onready var smoking = $"../../Body/AnimationPlayer"
 
+=======
+>>>>>>> 3c13fc0 (textures and state)
 var idle_time : float
 var player: CharacterBody3D
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+<<<<<<< HEAD
 
 func random_idle_time():
 	idle_time = randf_range(3,9)
 	smoking.play("03_smoking_standing")
 	
+=======
+func random_idle_time():
+	idle_time = randf_range(3,4)
+>>>>>>> 3c13fc0 (textures and state)
 
 func Enter():
 	print("I went Idle")
@@ -31,7 +39,14 @@ func Update(delta: float):
 func Physics_Update(delta):
 	if not npc.is_on_floor():
 		npc.velocity.y -= gravity * delta
+<<<<<<< HEAD
 	#dvar direction = player.global_position - npc.global_position
 
 #	if direction.length() < 7:
 #		Transitioned.emit(self, "Follow")
+=======
+	var direction = player.global_position - npc.global_position
+
+	if direction.length() < 7:
+		Transitioned.emit(self, "Follow")
+>>>>>>> 3c13fc0 (textures and state)
