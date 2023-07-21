fcdba9efadb533e7424493a798bd631a1ada2ca0
AllenOpp <42706855+AllenOpp@users.noreply.github.com>
2023-07-20 20:06:26 -0500

    Dialogue Addon Implementation
    Iterating on dialogue manager

---
+++ Scripts/game_state.gd
@@ -0,0 +1,23 @@
+extends Node
+
+## Game Flags
+
+## Player Flags
+
+
+
+## Characters
+
+##################
+###### redman ####
+##################
+var have_met_red_man: bool = false
+var convo_value_red: String = ""
+var second_convo_red: bool = false
+
+##################
+##### blueman ####
+##################
+var have_met_blue_man: bool = false
+var convo_value_blue: String = ""
+var second_convo_blue: bool = false
